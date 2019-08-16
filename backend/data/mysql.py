92ea98fcbf4dc763589744491f36d8957279f613
Ayush Gupta <shgpt14@gmail.com>
2019-08-16 19:17:37 +0530

    sm changes

--- backend/data/mysql.py
+++ backend/data/mysql.py
@@ -11,13 +11,9 @@ from datetime import date
 app = Flask(__name__)
 app.secret_key = "ayush" 
 
-<<<<<<< HEAD
+
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-=======
-app.config['MYSQL_DATABASE_USER'] = 'ayush'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
 app.config['MYSQL_DATABASE_DB'] = 'quizzinga'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
@@ -28,41 +24,24 @@ jwt = JWTManager(app)
 
 CORS(app)
 
-<<<<<<< HEAD
+
 @app.route('/user/register', methods=['POST'])
-=======
-@app.route('/student/register', methods=['POST'])
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
 def register():
     conn=mysql.connect()
     cur = conn.cursor()
     first_name = request.get_json()['first_name']
     last_name = request.get_json()['last_name']
     email = request.get_json()['email']
-<<<<<<< HEAD
     branch = "CSE"
     college_id = 1
-=======
-    branch = request.get_json()['branch']
-    college_id = request.get_json()['college_id']
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
     password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
     created = datetime.utcnow()
     photo=None
 	
-<<<<<<< HEAD
     cur.execute("INSERT INTO user (f_name, l_name, email, password,photo,college_id) VALUES ('" + 
-=======
-    cur.execute("INSERT INTO student (f_name, l_name, email, password,branch,photo,college_id) VALUES ('" + 
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
-		str(first_name) + "', '" + 
 		str(last_name) + "', '" + 
 		str(email) + "', '" + 
         str(password) + "', '" +
-<<<<<<< HEAD
-=======
-        str(branch) + "', '" +  
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
         str(photo) + "', '" + 
 		str(college_id) + "' )")
     conn.commit()
@@ -77,51 +56,21 @@ def register():
     return jsonify({'result' : result})
 	
 
-<<<<<<< HEAD
+
 @app.route('/user/login', methods=['POST'])
-=======
-@app.route('/student/login', methods=['POST'])
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
 def login():
     cur = mysql.connect().cursor()
     email = request.get_json()['email']
     password = request.get_json()['password']
     result = ""
 	
-<<<<<<< HEAD
     cur.execute("SELECT * FROM user where email = '" + str(email) + "'")
-=======
-    cur.execute("SELECT * FROM student where email = '" + str(email) + "'")
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
     rv = cur.fetchone()
 	
     if bcrypt.check_password_hash(rv[4], password):
         access_token = create_access_token(identity = {'first_name': rv[1],'last_name': rv[2],'email': rv[3]})
         result = access_token
-<<<<<<< HEAD
         session['id']=rv[0]
-=======
-        session['token']=result
-    else:
-        result = jsonify({"error":"Invalid username and password"})
-    
-    return result
-
-@app.route('/faculty/login', methods=['POST'])
-def faclogin():
-    cur = mysql.connect().cursor()
-    email = request.get_json()['email']
-    password = request.get_json()['password']
-    result = ""
-    
-    cur.execute("SELECT * FROM faculty where email = '" + str(email) + "'")
-    rv = cur.fetchone()
-    
-    if rv is not None and bcrypt.check_password_hash(rv[4], password):
-        access_token = create_access_token(identity = {'first_name': rv[1],'last_name': rv[2],'email': rv[3]})
-        result = access_token
-        session['token']=result
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
     else:
         result = jsonify({"error":"Invalid username and password"})
     
@@ -129,7 +78,6 @@ def faclogin():
 
 @app.route('/student/logout', methods=['POST'])
 def logout():
-<<<<<<< HEAD
     session.pop('id',None)
     return "user is logged out"
 
@@ -150,52 +98,12 @@ def getdashboard():
     cur.execute("SELECT * FROM user_participate_quiz,quiz where user_participate_quiz.u_id='" + str(u_id) +"' and quiz.quiz_id=user_participate_quiz.quiz_id and quiz.startTime<=CURRENT_TIMESTAMP ")
     past_quiz=cur.fetchall()
     return jsonify([past_quiz,upcoming_quiz,registered_quiz])
-=======
-    session.pop('token',None)
-    return "user is logged out"
 
-@app.route('/student/dashboard', methods=['POST'])
-def studashboard():
-    conn=mysql.connect()
-    cur = conn.cursor()
-    st_id = request.get_json()['stud_id']
-    #all quizes
-    cur.execute("SELECT * FROM quiz")
-    rv=cur.fetchall()
-    rows={}
-    i=0
-    for row in rv:
-        i+=1
-        rows.update({"row"+str(i):row[1]})
-    #upcoming quiz(all)
-    today=date.today()
-    cur.execute("SELECT * FROM student_participate_quiz,quiz where student_id='" + str(st_id) +"' and quiz.quiz_id=student_participate_quiz.quiz_id and quiz.startTime>CURRENT_TIMESTAMP ")
-    upcoming_quiz=cur.fetchall()
-    rows={}
-    i=0
-    for row in upcoming_quiz:
-        i+=1
-        rows.update({"row"+str(i):row[1]})
-    #past quizzes (registered)
-    cur.execute("SELECT * FROM student_participate_quiz,quiz where student_id='" + str(st_id) +"' and quiz.quiz_id=student_participate_quiz.quiz_id and quiz.startTime<=CURRENT_TIMESTAMP ")
-    past_quiz=cur.fetchall()
-    rows={}
-    i=0
-    for row in past_quiz:
-        i+=1
-        rows.update({"row"+str(i):row[0]})
 
-    return jsonify(rows)
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
-    
-    #upcoming registered quiz
     
 @app.route('/student/profile', methods=['POST'])
-<<<<<<< HEAD
+
 def agetProfile():
-=======
-def getProfile():
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
     conn=mysql.connect()
     cur = conn.cursor()
     st_id = request.get_json()['stud_id']
@@ -203,7 +111,6 @@ def getProfile():
     rv=cur.fetchone()
     return jsonify({"result":rv[2]})
 
-<<<<<<< HEAD
 
 @app.route('/user/myQuiz', methods=['GET'])
 def getQuiz():
@@ -215,7 +122,7 @@ def getQuiz():
     cur.execute("SELECT * from quiz where u_id='"+ str(u_id)+"' ")
     rv=cur.fetchall()
     return jsonify(rv)
-=======
+
 @app.route('/faculty/profile', methods=['POST'])
 def getfacProfile():
     conn=mysql.connect()
@@ -224,16 +131,6 @@ def getfacProfile():
     cur.execute("SELECT * from faculty where f_id='"+ str(fac_id)+"'")
     rv=cur.fetchone()
     return jsonify({"result":rv[2]})
-
-@app.route('/faculty/myQuiz', methods=['POST'])
-def getQuiz():
-    conn=mysql.connect()
-    cur = conn.cursor()
-    fac_id = request.get_json()['fac_id']
-    cur.execute("SELECT * from quiz where f_id='"+ str(fac_id)+"'")
-    rv=cur.fetchall()
-    return jsonify({"result":rv[0]})
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
 
 @app.route('/faculty/createQuiz', methods=['POST'])
 def createQuiz():
@@ -338,7 +235,6 @@ def startQuiz():
     rv=cur.fetchone()
     return jsonify({"result":rv})
 
-<<<<<<< HEAD
 @app.route('/user/myQuiz', methods=['POST'])
 def myQuiz():
     conn=mysql.connect()
@@ -403,8 +299,3 @@ def participate():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 9d9832043aba2e7ef4749521ab6f95c57f83f3f0
